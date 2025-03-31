252e10eab68d0a1fa393c67ad75594aed3cc9eac
gustasvs <jakensjake@gmail.com>
2025-04-01 01:07:02 +0300

    experiment with 3d cnn models

--- model/model.py
+++ model/model.py
@@ -3,10 +3,10 @@ from torch import nn
 from torch.nn import functional as F
 from torch.optim import Adam
 
-from torchvision.models import resnet50, ResNet50_Weights, resnet18, resnet34, efficientnet_b1, efficientnet_b3, efficientnet_v2_s, efficientnet_v2_m
+from torchvision.models import resnet50, ResNet50_Weights, resnet18, efficientnet_b0, efficientnet_b1, efficientnet_b3, efficientnet_v2_s, efficientnet_b2
 from torchvision.transforms import Compose, ToTensor, Normalize
-from torchvision.models.video.s3d import S3D
-from torchvision.models.video.resnet import mc3_18, r3d_18
+from torchvision.models.video.s3d import s3d
+from torchvision.models.video.resnet import r2plus1d_18
 
 from functions.settings import *
 
@@ -14,43 +14,65 @@ from functions.settings import *
 class VideoModel(nn.Module):
     def __init__(self):
         super(VideoModel, self).__init__()
-        # resnet = resnet18()
-        efficientnet = efficientnet_v2_s()
-        print(efficientnet.features[0][0])
-        # efficientnet = efficientnet_b3()
-        # print(resnet)
-        if GRAYSCALE:
-            # resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
-            efficientnet.features[0][0] = nn.Conv2d(1, 24, kernel_size=3, stride=2, padding=1, bias=False)
-        # self.feature_extractor = nn.Sequential(*list(resnet.children())[:-1])
-        # self.feature_dim = resnet.fc.in_features  # typically 512
-        self.feature_extractor = nn.Sequential(*list(efficientnet.children())[:-1])
+
+        efficientnet = efficientnet_b2()
+
         self.feature_dim = efficientnet.classifier[1].in_features
 
-        print(self.feature_dim)
+        efficientnet.features[0][0] = nn.Conv2d(TEMPORAL_FRAME_WINDOW, 32, kernel_size=3, stride=2, padding=1, bias=False)
+        # Remove LSTM layers and update classifier:
+        self.classifier = nn.Linear(self.feature_dim, TARGET_CLASS_COUNT)
+
+        self.feature_extractor = nn.Sequential(*list(efficientnet.children())[:-1])
+
+        # # resnet = resnet18()
+        # # efficientnet = efficientnet_v2_s() # 24
+        # # efficientnet = efficientnet_b4() # 48
+        # # efficientnet = efficientnet_b3() # 40
+        # # efficientnet = efficientnet_b1() # 32
+        # # efficientnet = efficientnet_b2() # 32
+        # efficientnet = efficientnet_b0()
+        # # print(efficientnet.features[0][0])
         
-        # LSTM to process sequential features from each frame
-        self.lstm = nn.LSTM(input_size=self.feature_dim, hidden_size=128,
-                            num_layers=3, batch_first=True)        
-        # Final classifier layer
-        self.classifier = nn.Linear(128, TARGET_CLASS_COUNT)
+        # # print(resnet)
+        # if GRAYSCALE:
+        #     # resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        #     efficientnet.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
+        # # self.feature_extractor = nn.Sequential(*list(resnet.children())[:-1])
+        # # self.feature_dim = resnet.fc.in_features  # typically 512
+        # self.feature_extractor = nn.Sequential(*list(efficientnet.children())[:-1])
+        # self.feature_dim = efficientnet.classifier[1].in_features
+        
+        # # LSTM to process sequential features from each frame
+        # self.lstm1 = nn.LSTM(input_size=self.feature_dim, hidden_size=128, num_layers=1, batch_first=True)
+        # self.lstm2 = nn.LSTM(input_size=128, hidden_size=96, num_layers=1, batch_first=True)
+        # self.lstm3 = nn.LSTM(input_size=96, hidden_size=64, num_layers=1, batch_first=True)
+        # # Final classifier layer
+        # self.classifier = nn.Linear(64, TARGET_CLASS_COUNT)
 
 
     def forward(self, x):
-        # x shape: (batch_size, num_frames, C, H, W)
-        batch_size, num_frames, C, H, W = x.size()
-        
-        # Reshape to (batch_size*num_frames, C, H, W) for CNN processing
-        x = x.view(batch_size * num_frames, C, H, W)
-        features = self.feature_extractor(x)  # output shape: (batch_size*num_frames, feature_dim, 1, 1)
-        features = features.view(batch_size, num_frames, self.feature_dim)
-        
-        # Process the sequence of features with LSTM
-        lstm_out, _ = self.lstm(features)  # lstm_out shape: (batch_size, num_frames, hidden_dim)
-        
-        # Use the output from the final time step for classification
-        out = self.classifier(lstm_out[:, -1, :])
+        # # x shape: (batch_size, num_frames, C, H, W)
+        x = x.squeeze(2)  # Now x shape: (batch_size, num_frames, H, W)
+        # Pass the tensor directly; num_frames is used as the channel dimension
+        features = self.feature_extractor(x)  # output shape: (batch_size, self.feature_dim, 1, 1)
+        features = features.view(features.size(0), -1)
+        out = self.classifier(features)
         return out
+
+        # # x shape: (batch_size, num_frames, C, H, W)
+        # batch_size, num_frames, C, H, W = x.size()
+        # # Reshape to (batch_size*num_frames, C, H, W) for CNN processing
+        # x = x.view(batch_size * num_frames, C, H, W)
+        # features = self.feature_extractor(x)  # output shape: (batch_size*num_frames, feature_dim, 1, 1)
+        # features = features.view(batch_size, num_frames, self.feature_dim)
+        # # Process the sequence of features with LSTM
+        # out, _ = self.lstm1(features)
+        # out, _ = self.lstm2(out)
+        # lstm_out, _ = self.lstm3(out)
+        # # Use the output from the final time step for classification
+        # out = self.classifier(lstm_out[:, -1, :])
+        # return out
 
 class Model(nn.Module):
     def __init__(self):
@@ -69,6 +91,53 @@ class Model(nn.Module):
     def forward(self, x):
         return self.model(x)
     
+
+class r21d(nn.Module):
+    def __init__(self):
+        super(r21d, self).__init__()
+        r2plus1d = r2plus1d_18()
+
+        self.feature_dim = r2plus1d.fc.in_features
+
+        if GRAYSCALE:
+            r2plus1d.stem[0] = nn.Conv3d(1, 45, kernel_size=(1, 7, 7), stride=(1, 2, 2), padding=(0, 3, 3), bias=False)
+        
+        r2plus1d.fc = nn.Identity()  # Remove the final classification layer
+        self.model = nn.Sequential(
+            r2plus1d,
+            nn.Linear(self.feature_dim, TARGET_CLASS_COUNT)
+        )
+
+    def forward(self, x):
+        # x shape: (batch_size, num_frames, C, H, W)
+        # Reshape to (batch_size, C, num_frames, H, W) for 3D CNN processing
+        batch_size, num_frames, C, H, W = x.size()
+        x = x.view(batch_size, C, num_frames, H, W)
+        return self.model(x)
+
+class VideoModel3sD(nn.Module):
+    def __init__(self):
+        super(VideoModel3sD, self).__init__()
+        self.model = s3d(dropout = 0.5)
+        
+        if GRAYSCALE:
+            self.model.features[0][0][0] = nn.Conv3d(1, 64, kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=(0, 5, 5), bias=False)
+        
+        # overwrite the final classifier layer
+        self.model.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        self.model.classifier = nn.Sequential(
+            torch.nn.Dropout(p=0.2, inplace=False),
+            torch.nn.Conv3d(1024, TARGET_CLASS_COUNT, kernel_size=(1, 1, 1), stride=(1, 1, 1))
+        )
+
+    def forward(self, x):
+        # x shape: (batch_size, num_frames, C, H, W)
+        # Reshape to (batch_size, C, num_frames, H, W) for 3D CNN processing
+        batch_size, num_frames, C, H, W = x.size()
+        x = x.view(batch_size, C, num_frames, H, W)
+        x = self.model(x)
+        return x
+
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=100):
